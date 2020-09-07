ab5f1876faf177daab634494884ecd3adb683435
augus <augus@DESKTOP-EUP9QV9>
2020-09-07 02:46:28 +0200

    test

--- Innlevering1/src/no/hvl/dat100/Oppgave4.java
+++ Innlevering1/src/no/hvl/dat100/Oppgave4.java
@@ -2,4 +2,11 @@ package no.hvl.dat100;
 
 public class Oppgave4 {
 
+	public static void main(String[] args) {
+		
+		System.out.println("Nytt prosjekt fra Github!");
+
+		System.out.println("Nytt prosjekt fra aUGHUST!");
+	}
+
 }
