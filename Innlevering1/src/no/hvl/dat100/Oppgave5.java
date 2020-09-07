9d3faabe0f4d5a51bf5fed62e67f4644630686e3
augus <augus@DESKTOP-EUP9QV9>
2020-09-07 19:04:52 +0200

    Karakterer

--- Innlevering1/src/no/hvl/dat100/Oppgave5.java
+++ Innlevering1/src/no/hvl/dat100/Oppgave5.java
@@ -1,5 +1,41 @@
 package no.hvl.dat100;
 
-public class Oppgave5 {
+import static javax.swing.JOptionPane.*;
 
-}
+public class Oppgave5{
+	
+	public static void main(String[] args) {
+		
+		for(int i= 0; i < 9; i++) {
+			int poeng = Integer.parseInt(showInputDialog("Tast inn Poeng"));
+			char karakter = ' ';
+		if (poeng >= 90 && poeng <= 100) {
+			karakter = 'A';
+			showMessageDialog(null, "Eleven fikk karakteren:" + karakter);
+		}
+		else if (poeng >= 80 && poeng <= 89) {
+			karakter = 'B';
+			showMessageDialog(null, "Eleven fikk karakteren:" + karakter);
+		}
+		else if (poeng >= 60 && poeng <= 79) {
+			karakter = 'C';
+			showMessageDialog(null, "Eleven fikk karakteren:" + karakter);
+		}
+		else if (poeng >= 50 && poeng <= 59) {
+			karakter = 'D';
+			showMessageDialog(null, "Eleven fikk karakteren:" + karakter);
+		}
+		else if (poeng >= 40 && poeng <= 49) {
+			karakter = 'E';
+			showMessageDialog(null, "Eleven fikk karakteren:" + karakter);
+		}
+		else if (poeng >= 0 && poeng <= 39) {
+			karakter = 'F';
+			showMessageDialog(null, "Eleven fikk karakteren:" + karakter);
+		}
+		else if (poeng >= 100 || poeng < 0) {
+			showMessageDialog(null, "Feilmelding: Ugyldig poengsum.");
+		}
+		}
+		
+	
